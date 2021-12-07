58bb19be952989f6309ddcdafd2ca22905aff35c
Giedrius Zavisa <giedrius.zavisa@gmail.com>
2021-12-07 15:06:57 +0200

    port settings updated

--- server.js
+++ server.js
@@ -36,6 +36,6 @@ app.get('/profile/:id', (req, res) => { profile.handleprofileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)});
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)});
 
-app.listen(3000, () => {
-    console.log('app is running');
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`app is running on ${process.env.PORT}`);
 });
