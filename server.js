0c99e3fe9733b198613c015b1068eff11247b9a9
Giedrius Zavisa <giedrius.zavisa@gmail.com>
2021-12-07 14:42:06 +0200

    files

---
+++ server.js
@@ -0,0 +1,41 @@
+// Modules and imports
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const knex = require('knex')
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+
+
+// Definitions
+const db = knex({
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        user: 'giedrius',
+        password: '656894',
+        database: 'smart-brain'
+    }
+});
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+// Handlers
+app.get('/', (req, res)=> {res.json('success')});
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt)});
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) });
+app.get('/profile/:id', (req, res) => { profile.handleprofileGet(req, res, db)});
+app.put('/image', (req, res) => { image.handleImage(req, res, db)});
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)});
+
+app.listen(3000, () => {
+    console.log('app is running');
+});
